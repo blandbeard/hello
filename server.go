a0b493f6c0d8fd480b5db3f3ad524b315f243f98
Blandbeard <blandbeard@users.noreply.github.com>
2013-12-25 01:35:17 -0500

    Form submitting rendering response

--- server.go
+++ server.go
@@ -3,7 +3,13 @@ package main
 import (
   "github.com/codegangsta/martini"
   "github.com/codegangsta/martini-contrib/render"
+  "github.com/codegangsta/martini-contrib/binding"
 )
+
+type Registration struct {
+  Uuid  string  `form:"uuid" json:"uuid" binding:"required"`
+}
+
 
 func main() {
   m := martini.Classic()
@@ -13,6 +19,11 @@ func main() {
 
   m.Get("/", func(r render.Render) {
     r.HTML(200, "index", "blandbeard")
+  })
+
+  m.Post("/registrations", binding.Bind(Registration{}), func(registration Registration, r render.Render) string {
+    r.HTML(200, "registrations", registration.Uuid)
+    return registration.Uuid
   })
 
   m.Run()
